b2b7d04d1f73909a67e2fcf0419608f6a20829ea
Eduardo Flores <eduardo@enelmargen.org>
2017-01-23 17:15:44 -0600

    add maps viz library v1

---
+++ R/tema.R
@@ -0,0 +1,21 @@
+#' Funcion generica para temas
+#'
+#' Temas para graficas (ggplot, ploty o highcharter)
+#'
+#' @param tipo tipo de grafica (hc, ggplot, o plotly)
+#' @param colores esquema de colores, generalmente el default (tse1)
+#' @export
+tse_theme <- function(tipo = "hc") {
+  if(tipo == "hc"){
+
+
+  }
+
+  if(tipo == "ggplot"){
+
+  }
+
+  if(tipo == "plotly"){
+
+  }
+}
